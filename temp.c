0d21479d467c24359b99c59b64b0c05b3739aece
Martell94 <116161025+Martell94@users.noreply.github.com>
2022-12-29 17:55:05 +0100

    Update temp.c

--- temp.c
+++ temp.c
@@ -1,15 +1,15 @@
 #include "header.h"
 
-void init_temps(temp_t* self, const uint8_t pin){
-	
+void init_temps(temp_t* self, const uint8_t pin)
+{
 	self->pin = pin;
 	init_adc(self);
-	// Initiera seriell överföring, 9600 kbps.
+	// Initiera seriell Ã¶verfÃ¶ring, 9600 kbps.
 	return;
 }
 
-void get_temp(temp_t* self){
-	
+void get_temp(temp_t* self)
+{
 	const double input_voltage = adc_getv(self) /1023.0 * 5.0;
 	self->temp = (input_voltage * 100) - 50;
 	
@@ -17,9 +17,8 @@ void get_temp(temp_t* self){
 }
 
 
-void print_temp(temp_t* self){
-	
-	
+void print_temp(temp_t* self)
+{
 	
 	return;
 }
